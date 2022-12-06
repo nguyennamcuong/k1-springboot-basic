c6e59a47cab09ae6063173b2e792f27df696c47f
nguyennamcuong <cuong@gmail.com>
2022-12-07 06:48:09 +0700

    Add lession 2

---
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystem/Lession2/MovieFilter.java
@@ -0,0 +1,2 @@
+package com.nbstech.spring.basic.movierecommendersystem.Lession2;public interface MovieFilter {
+}
--- src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovierecommendersystemApplication.java
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovierecommendersystemApplication.java
@@ -1,13 +1,25 @@
 package com.nbstech.spring.basic.movierecommendersystem;
 
+import com.nbstech.spring.basic.movierecommendersystem.Lession1.Movierecommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MovierecommendersystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovierecommendersystemApplication.class, args);
-	}
+//SpringApplication.run(MovieRecommenderSystemApplication.class, args);
 
+		//create object of RecommenderImplementation class
+		Movierecommender recommender = new Movierecommender();
+
+		//call method to get recommendations
+		String[] result = recommender.recommendMovies("Finding Dory");
+
+		//display results
+		System.out.println(Arrays.toString(result));
+
+	}
 }
