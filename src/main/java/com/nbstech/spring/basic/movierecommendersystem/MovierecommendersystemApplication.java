a09e90722c7428c4153bb5653187f6e8f1e1bf0e
nguyennamcuong <cuong@gmail.com>
2022-12-14 06:52:44 +0700

    Config app with ComponentScan

--- src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovierecommendersystemApplication.java
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovierecommendersystemApplication.java
@@ -1,25 +1,27 @@
 package com.nbstech.spring.basic.movierecommendersystem;
 
 import com.nbstech.spring.basic.movierecommendersystem.Lession3.Movierecommender;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
-@SpringBootApplication
+//@SpringBootApplication
+@ComponentScan
 public class MovierecommendersystemApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(MovieRecommenderSystemApplication.class, args);
-		ApplicationContext appContext = SpringApplication.run(MovierecommendersystemApplication.class, args);
-
+//		ApplicationContext appContext = SpringApplication.run(MovierecommendersystemApplication.class, args);
+		AnnotationConfigApplicationContext appContext =
+				new AnnotationConfigApplicationContext(MovierecommendersystemApplication.class);
 		Movierecommender recommender = appContext.getBean(Movierecommender.class);
 
 		String[] result = recommender.recommendMovies("Finding Dory");
 
 		//display results
 		System.out.println(Arrays.toString(result));
+		appContext.close();
 
 	}
 }
